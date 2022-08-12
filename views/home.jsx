8369b9a568ded2f017e51cb1b0a0168c69b0c1e5
unknown <thomaspchristian@outlook.com>
2022-08-11 21:02:03 -0400

    styling

--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,30 @@
-const React = require('react')
-const Default = require('./default')
+const React = require("react");
+const Default = require("./default");
 
-function Home () {
-    return (
-      <Default>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Default>
-    )
-  }
+function Home() {
+  return (
+    <Default>
+      <main>
+        <h1>HOME</h1>
+        <div id="home">
+          <img src="/images/homepage.jpg" alt="Food" height={500} />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@simplethemes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Casey Lee
+            </a>{" "}
+            on
+            <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+          </div>
+          <a href="/places">
+            <button className="btn-primary">Places Page</button>
+          </a>
+        </div>
+      </main>
+    </Default>
+  );
+}
 
-module.exports = Home
+module.exports = Home;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,19 +2,28 @@ const React = require('react')
 const Default = require('../default')
 
 function Index (data) {
-    let placesFormatted = data.places.map((place) => {
-      return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
-        </div>
-      )
-    })
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div className="col-sm-6">
+        <h2 className='text-center'>{place.name}</h2>
+        <p className="text-center">
+          {place.cuisines}
+        </p>
+        <img src={place.pic} className="rounded mx-auto d-block" alt={place.name} height="200" />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+  })
+  
     return (
       <Default>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
+              <h1>Places People Enjoy</h1>
+              <div className='row'>
               {placesFormatted}
+              </div>
           </main>
       </Default>
   )
