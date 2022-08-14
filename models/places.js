9b07eb6dfc7a60b949584ab69be28fa22a4b6813
unknown <thomaspchristian@outlook.com>
2022-08-14 11:49:39 -0400

    added delete/edit

---
+++ models/places.js
@@ -0,0 +1,15 @@
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cafe-business.jpg'
+  }]
+
+  module.exports = places
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,16 +1,24 @@
-const React = require('react')
-const Default = require('./default')
+const React = require("react");
+const Default = require("../default");
 
-function Show () {
+function Show(data) {
+  return (
+    <Default>
+      <main>
+        <h1>{data.place.name}</h1>
+      </main>
 
-    return (
-          <Default>
-              <main>
-                  <h1>SHOW</h1>
-              </main>
-          </Default>
-        )
-      
+      <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        Edit
+      </a>
+
+      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <button type="submit" className="btn btn-danger">
+          Delete
+        </button>
+      </form>
+    </Default>
+  );
 }
 
-module.exports = Show
+module.exports = Show;
